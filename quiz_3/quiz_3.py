933e52f2a54f4854ed221fbc0e775564686a772a
wangjinchen <wangjinchen2218@icloud.com>
2019-10-25 01:09:02 +1100

    Add files via upload

---
+++ quiz_3/quiz_3.py
@@ -0,0 +1,38 @@
+# COMP9021 19T3 - Rachid Hamadi
+# Quiz 3 *** Due Thursday Week 4
+
+
+# Reading the number written in base 8 from right to left,
+# keeping the leading 0's, if any:
+# 0: move N     1: move NE    2: move E     3: move SE
+# 4: move S     5: move SW    6: move W     7: move NW
+#
+# We start from a position that is the unique position
+# where the switch is on.
+#
+# Moving to a position switches on to off, off to on there.
+
+import sys
+
+on = '\u26aa'
+off = '\u26ab'
+code = input('Enter a non-strictly negative integer: ').strip()
+try:
+    if code[0] == '-':
+        raise ValueError
+    int(code)
+except ValueError:
+    print('Incorrect input, giving up.')
+    sys.exit()
+nb_of_leading_zeroes = 0
+for i in range(len(code) - 1):
+    if code[i] == '0':
+        nb_of_leading_zeroes += 1
+    else:
+        break
+print("Keeping leading 0's, if any, in base 8,", code, 'reads as',
+      '0' * nb_of_leading_zeroes + f'{int(code):o}.'
+     )
+print()
+
+# INSERT YOUR CODE HERE
